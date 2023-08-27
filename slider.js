4d0be8643a6c1a7e0cdc7b62f8c63cc08f283b92
akshat <akshatbhatya@gmail.com>
2023-08-27 09:00:45 +0530

    all is done

--- slider.js
+++ slider.js
@@ -1,13 +1,43 @@
-const imagesslides=document.querySelectorAll('.slide')
-let counter=0;
+const imagesslides = document.querySelectorAll('.slide');
+console.log(imagesslides.length);
+
+const previousbtn = document.querySelector('.prev');
+const nextbtn = document.querySelector('.next')
+var counter = 0;
 
 console.log(imagesslides);
-imagesslides.forEach((slide,index)=>{
-    slide.style.left=`${index*100}%`
+imagesslides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`
 
 })
 
-function slideimage(slide){
-    slide.style.transform=`translateX(-(${counter*100}%))`
 
+
+
+
+nextbtn.addEventListener('click', () => {
+    counter++
+    if(counter==imagesslides.length){
+        console.log(counter,"inner counter");
+        counter=0;
+    }
+    console.log(counter);
+    slideimage()
+})
+
+previousbtn.addEventListener('click', () => {
+    counter--;
+    if (counter==-1){
+        console.log(counter,"inner counter");
+        counter=imagesslides.length-1;
+        
+    }
+    console.log(counter);
+    slideimage()
+})
+
+function slideimage() {
+    imagesslides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    })
 }
