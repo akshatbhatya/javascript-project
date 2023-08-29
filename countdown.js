b01fa4e4dab7b09b58a2341075f42b6076c6672d
akshat <akshatbhatya@gmail.com>
2023-08-29 16:58:16 +0530

    days and time is correct only seconds are wrong work

---
+++ countdown.js
@@ -0,0 +1,43 @@
+function timecalculation() {
+    const time = "21 sep 2023 12:00 PM";
+    const futuredate = new Date(time);
+    console.log(futuredate);
+    const nowdate = new Date();
+    console.log(nowdate);
+
+    let totalTimeLeft = futuredate - nowdate;
+    console.log(totalTimeLeft);
+
+
+    const onedays = 24 * 60 * 60 * 1000;
+    const onehours = 60 * 60 * 1000;
+    const oneminutes = 60 * 1000;
+    const oneseconds = 1000;
+    const days = totalTimeLeft / onedays;
+
+    var futureDays = Math.round(days);
+    console.log(futureDays);
+    const hours = totalTimeLeft / onehours;
+    var futurehours = Math.round(hours);
+    const minutes = totalTimeLeft / oneminutes;
+    var futureminutes = Math.round(minutes);
+    const seconds = totalTimeLeft / oneseconds;
+    var futureseconds = Math.round(seconds)%60;
+
+
+    const day = document.querySelector('.days');
+    const ourhours = document.querySelector('.hours');
+    const ourminutes = document.querySelector('.minutes');
+    const ourseconds = document.querySelector('.seconds');
+    ourseconds.innerHTML = futureseconds;
+    ourminutes.innerHTML = futureminutes;
+    ourhours.innerHTML = futurehours;
+    day.innerHTML = futureDays;
+
+}
+
+
+
+setInterval(() => {
+    timecalculation();
+}, 1000);
